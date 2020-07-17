97109876aaa11e41f983dfc293c32077aac8c52d
Niraj <amatyaraj@gmail.com>
2020-07-17 15:12:46 +1000

    dummy data created

---
+++ src/data/meal_data.js
@@ -0,0 +1,35 @@
+export default [
+	{
+		mealType: 'Dinner',
+		_id: 1,
+		title: 'Burger',
+		description: 'Home made',
+		deliversOn: '2020-07-15T00:00:00.000Z',
+		orderStarts: '2020-07-12T00:00:00.000Z',
+		orderEnds: '2020-07-16T00:00:00.000Z',
+		maxOrders: 20,
+		cost: 20
+	},
+	{
+		mealType: 'Dinner',
+		_id: 2,
+		title: 'Tandoori Chicken',
+		description: 'Spicy homemade meal',
+		deliversOn: '2020-07-17T16:30:00.000Z',
+		orderStarts: '2020-07-1710:00:00.000Z',
+		orderEnds: '2020-07-16T14:30:00.000Z',
+		maxOrders: 15,
+		cost: 35
+	},
+	{
+		mealType: 'Dinner',
+		_id: 2,
+		title: 'Noodle Soup',
+		description: 'Noodle with hot soup',
+		deliversOn: '2020-07-14T16:30:00.000Z',
+		orderStarts: '2020-07-1410:00:00.000Z',
+		orderEnds: '2020-07-14T14:30:00.000Z',
+		maxOrders: 15,
+		cost: 35
+	}
+];

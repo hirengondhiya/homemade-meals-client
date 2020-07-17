813870f961301487c4833d4a7485213f1ab3426e
Niraj <amatyaraj@gmail.com>
2020-07-17 15:17:53 +1000

    added dummy data for meals

--- src/data/meal_data.js
+++ src/data/meal_data.js
@@ -1,14 +1,14 @@
 export default [
 	{
-		mealType: 'Dinner',
+		mealType: 'Lunch',
 		_id: 1,
 		title: 'Burger',
 		description: 'Home made',
-		deliversOn: '2020-07-15T00:00:00.000Z',
-		orderStarts: '2020-07-12T00:00:00.000Z',
-		orderEnds: '2020-07-16T00:00:00.000Z',
+		deliversOn: '2020-07-15T10:30:00.000Z',
+		orderStarts: '2020-07-12T08:30:00.000Z',
+		orderEnds: '2020-07-16T10:00:00.000Z',
 		maxOrders: 20,
-		cost: 20
+		cost: 12
 	},
 	{
 		mealType: 'Dinner',
@@ -31,5 +31,16 @@ export default [
 		orderEnds: '2020-07-14T14:30:00.000Z',
 		maxOrders: 15,
 		cost: 35
+	},
+	{
+		mealType: 'Lunch',
+		_id: 1,
+		title: 'Spaghetti with fried pork',
+		description: 'Freshly cooked food for food lover',
+		deliversOn: '2020-07-15T10:30:00.000Z',
+		orderStarts: '2020-07-12T08:30:00.000Z',
+		orderEnds: '2020-07-16T10:00:00.000Z',
+		maxOrders: 20,
+		cost: 15
 	}
 ];

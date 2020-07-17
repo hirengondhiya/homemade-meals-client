65ce67ff0e30d01b790be32876c740c396567766
Niraj <amatyaraj@gmail.com>
2020-07-17 14:46:10 +1000

    imported router and stateReduce in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useReducer, useEffect } from 'react';
+import { BrowseRouter, Router } from 'react-router-dom';
+import stateReducer from './config/stateReducer';
+
+const intialState = {
+	meals: []
+};
+
+const [ store, dispatch ] = useReducer(stateReducer, initialState);
 
 const App = () => {
-  return (
-    <div >
-          The basic empty template
-    </div>
-  )
-}
+	return <div>The basic empty template</div>;
+};
 
-export default App
+export default App;

4c6b2bcafc0789fb157b67be472715b853c9f783
Niraj <amatyaraj@gmail.com>
2020-07-18 11:49:50 +1000

    added a helper function to get meal by id

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import AddNewMeal from './components/meals-components/AddNewMeal';
+import ViewMeal from './components/meals-components/ViewMeal';
 
 const App = () => {
 	const [ meals, setMeals ] = useState([]);
 
+	// returns the meal of the id provided
+	function getMealFromID(id) {
+		const meal = meals.find((meal) => meal.id === id);
+	}
+
+	// Add a new meal
 	function addMeal(newMeal) {
 		setMeals([ ...meals, newMeal ]);
 	}
@@ -15,6 +22,11 @@ const App = () => {
 			<BrowserRouter>
 				<h1>Homemade Meals</h1>
 				<Route exact path="/meals/new" render={(props) => <AddNewMeal {...props} addMeal={addMeal} />} />
+				<Route
+					exact
+					path="/meals/:id"
+					render={(props) => <ViewMeal {...props} meal={getMealFromID(props.match.params.id)} />}
+				/>
 			</BrowserRouter>
 		</div>
 	);
--- src/components/meals-components/Meals.js
+++ src/components/meals-components/Meals.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Meal from './Meal';
-import { useGlobalState } from '../config/store';
+import { useGlobalState } from '../../config/store';
 
 const Meal = () => {
 	const { store } = useGlobalState();
 	const { meals } = store;
+	return <div>{meals.sort((a, b))}</div>;
 };

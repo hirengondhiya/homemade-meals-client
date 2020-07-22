a89283dd891484942df2b5eb8ca64ebdf372e901
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-07-22 23:36:05 +1000

    reformatting code

--- src/App.js
+++ src/App.js
@@ -41,72 +41,70 @@ const App = () => {
     return () => { }
   }, [])
 
-	// returns the meal of the id provided
-	function getMealFromID(id) {
-		let meal = meals.find((meal) => meal._id === id);
-		return meal;
-	}
+  const [meals, setMeals] = useState([]);
+  // const [ loggedInUser, setLoggedInUser ] = useState(null);
 
-	// Add a new meal
-	function addMeal(newMeal) {
-		setMeals([ ...meals, newMeal ]);
-	}
+  // returns the meal of the id provided
+  function getMealFromID(id) {
+    let meal = meals.find((meal) => meal._id === id);
+    return meal;
+  }
 
-	// delete blog post that matched id
-	function deleteMeal(id) {
-		const updatedMeal = meals.find((meal) => meal._id !== id);
-		return updatedMeal;
-	}
+  // Add a new meal
+  function addMeal(newMeal) {
+    setMeals([...meals, newMeal]);
+  }
 
-	// update meal that match id
-	function updateMeal(updatedMeal) {
-		const otherMeal = meals.filter((meal) => meal._id !== updatedMeal._id);
-		setMeals([ ...otherMeal, updatedMeal ]);
-	}
-	// function to register user
-	function registerUser(user) {
-		setLoggedInUser(user.username);
-	}
+  // delete blog post that matched id
+  function deleteMeal(id) {
+    const updatedMeal = meals.find((meal) => meal._id !== id);
+    return updatedMeal;
+  }
 
-	// function to login user
-	function loginUser(user) {
-		setLoggedInUser(user.username);
-	}
+  // update meal that match id
+  function updateMeal(updatedMeal) {
+    const otherMeal = meals.filter((meal) => meal._id !== updatedMeal._id);
+    setMeals([...otherMeal, updatedMeal]);
+  }
+  // function to register user
+  // function registerUser(user) {
+  //   setLoggedInUser(user.username);
+  // }
 
-	return (
-		<div>
+  return (
+    <div>
       <StateContext.Provider value={{ store, dispatch }}>
-			<BrowserRouter>
-				<h1>Homemade Meals</h1>
-				<Switch>
-					<Route exact path="/" render={(props) => <Meals {...props} />} />
-					<Route exact path="/meals/new" render={(props) => <AddNewMeal {...props} addMeal={addMeal} />} />
-					<Route
-						exact
-						path="/meals/:id"
-						render={(props) => (
-							<ViewMeal
-								{...props}
-								meal={getMealFromID(props.match.params.id)}
-								showControls
-								deleteMeal={deleteMeal}
-							/>
-						)}
-					/>
-					<Route
-						exact
-						path="/meals/edit/:id"
-						render={(props) => (
-							<EditMeal {...props} updateMeal={updateMeal} meal={getMealFromID(props.match.params.id)} />
-						)}
-					/>
+        <BrowserRouter>
+          <h1>Homemade Meals</h1>
+          <Switch>
+            <Route exact path="/" render={(props) => <Meals {...props} />} />
+              <Route exact path="/meals/new" render={(props) => <AddNewMeal {...props} addMeal={addMeal} />} />
+              <Route
+                exact
+                path="/meals/:id"
+                render={(props) => (
+                  <ViewMeal
+                    {...props}
+                    meal={getMealFromID(props.match.params.id)}
+                    showControls
+                    deleteMeal={deleteMeal}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="/meals/edit/:id"
+                render={(props) => (
+                  <EditMeal {...props} updateMeal={updateMeal} meal={getMealFromID(props.match.params.id)} />
+                )}
+              />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
           </Switch>
         </BrowserRouter>
       </StateContext.Provider>
-		</div>
-	);
+    </div>
+  );
 };
 
 export default App;

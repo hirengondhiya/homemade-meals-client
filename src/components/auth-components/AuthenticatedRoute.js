7b6ed0316cb2dc2011d29fa7f6fb6c8b7a19f26c
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-07-24 23:29:43 +1000

    to protect routes

---
+++ src/components/auth-components/AuthenticatedRoute.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { useGlobalState } from '../../config/store'
+// import {getLoggedInUser} from '../../services/authServices'
+
+export default ({ component: Component, location, redirectMsg, ...rest }) => {
+  const { store } = useGlobalState()
+  const { loggedInUser } = store
+
+  return (
+    loggedInUser ?
+      <Route {...rest} location render={(props) => <Component {...props} />} /> :
+      <Redirect to={
+        {
+          pathname: '/login',
+          state: {
+            referrer: location.pathname,
+            msg: redirectMsg
+          }
+        }
+      } />
+  )
+}

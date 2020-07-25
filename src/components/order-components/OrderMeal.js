1ebe7954937f14ede56ba788580516f7abde90c7
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-07-25 12:44:03 +1000

    incorprated global state

--- src/components/order-components/OrderMeal.js
+++ src/components/order-components/OrderMeal.js
@@ -6,97 +6,105 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const OrderMeal = ({ history, meal, addOrder }) => {
-	function handleChange(event) {
-		const name = event.target.name;
-		const value = event.target.value;
-		setFormState({
-			...formState,
-			[name]: value
-		});
-	}
+import useGlobalState from '../../config/store'
 
-	function handleSubmit(event) {
-		event.preventDefault();
-		const newOrder = {
-			_id: '5f123edeb40acb71b7aaf7f3',
-			title: formState.title,
-			cost: parseInt(formState.cost),
-			pickupAt: formState.pickupAt,
-			quantity: parseInt(formState.quantity),
-			totalAmt: parseInt(meal.cost) * parseInt(formState.quantity)
-		};
-		addOrder(newOrder);
-		history.push(`/order/${newOrder._id}`);
-	}
+const OrderMeal = ({ history, match, addOrder }) => {
+  function handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormState({
+      ...formState,
+      [name]: value
+    });
+  }
 
-	const initialFormState = {
-		title: meal.title,
-		cost: parseInt(meal.cost),
-		pickupAt: '2020-07-20T22:08:11.000',
-		quantity: parseInt(1)
-	};
+  function handleSubmit(event) {
+    event.preventDefault();
+    const newOrder = {
+      mealId: meal._id,
+      pickupAt: new Date(formState.pickupAt).toISOString(),
+      quantity: parseInt(formState.quantity),
+      totalAmt: parseInt(meal.cost) * parseInt(formState.quantity)
+    };
+    // addOrder(newOrder);
+    dispatch({
+      type: 'setOrders',
+      data: [newOrder, ...store.orders]
+    })
+    history.push(`/order/${newOrder._id}`);
+  }
+  const { store, dispatch } = useGlobalState()
+  const { id } = (match && match.params) || {}  
 
-	const [ formState, setFormState ] = useState(initialFormState);
+  const meal = store.meals.find((meal) => meal._id === id)
 
-	return (
-		<Container>
-			<Row className="justify-content-center">
-				<Col lg={4}>
-					<Form onSubmit={handleSubmit}>
-						<h2>Order Meal</h2>
-						<Form.Group>
-							<Form.Label>Dish:</Form.Label>
-							<Form.Control type="text" id="title" name="title" value={formState.title} readOnly />
-						</Form.Group>
-						<Form.Group>
-							<Form.Label>Cost:</Form.Label>
-							<Form.Control type="text" id="cost" name="cost" value={formState.cost} readOnly />
-						</Form.Group>
-						<Form.Group>
-							<Form.Label htmlFor="pickupAt">Pickup Time (date and time):</Form.Label>
-							<Form.Control
-								required
-								type="datetime-local"
-								id="pickupAt"
-								name="pickupAt"
-								value={formState.pickupAt}
-								onChange={handleChange}
-							/>
-						</Form.Group>
-						<Form.Group>
-							<Form.Label htmlFor="quantity"> Max Order Quantity (between 1 and 50):</Form.Label>
-							<Form.Control
-								required
-								type="number"
-								id="quantity"
-								name="quantity"
-								min="1"
-								max="50"
-								value={formState.quantity}
-								onChange={handleChange}
-							/>
-						</Form.Group>
-						<Form.Group>
-							<Form.Label>Total:</Form.Label>
-							<Form.Control
-								type="number"
-								id="totalAmt"
-								name="totalAmt"
-								value={meal.cost * formState.quantity}
-								readOnly
-								// defaultValue={meal.cost}
-								// onChange={handleChange}
-							/>
-						</Form.Group>
-						<Button variant="primary" type="submit" className="mt-3" value="Confirm Order">
-							Confirm Order
+  const initialFormState = {
+    pickupAt: new Date(meal.deliversOn),
+    quantity: parseInt(1)
+  };
+  const [formState, setFormState] = useState(initialFormState);
+
+  if (!id || !meal) {
+    return (
+      <p className="text-danger mt-3">Can not find meal!</p>
+    )
+  }
+  return (
+    <Container>
+      <Row className="justify-content-center">
+        <Col lg={4}>
+          <Form onSubmit={handleSubmit}>
+            <h2>Order Meal</h2>
+            <Form.Group>
+              <Form.Label>Dish:</Form.Label>
+              <Form.Control type="text" id="title" name="title" value={meal.title} readOnly />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Cost:</Form.Label>
+              <Form.Control type="text" id="cost" name="cost" value={meal.cost} readOnly />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="pickupAt">Pickup Time (date and time):</Form.Label>
+              <Form.Control
+                required
+                type="datetime-local"
+                id="pickupAt"
+                name="pickupAt"
+                value={formState.pickupAt}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="quantity"> Max Order Quantity (between 1 and 50):</Form.Label>
+              <Form.Control
+                required
+                type="number"
+                id="quantity"
+                name="quantity"
+                min="1"
+                max="50"
+                value={formState.quantity}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Total:</Form.Label>
+              <Form.Control
+                type="number"
+                id="totalAmt"
+                name="totalAmt"
+                value={meal.cost * formState.quantity}
+                readOnly
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit" className="mt-3" value="Confirm Order">
+              Confirm Order
 						</Button>
-					</Form>
-				</Col>
-			</Row>
-		</Container>
-	);
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default OrderMeal;

cf497474d0041de60a28ebb635d5aad8a8f4e063
Niraj <amatyaraj@gmail.com>
2020-07-25 15:46:33 +1000

    added table and fixed the date issue
    Co-authored-by: Hiren Gondhiya <GCAS021910@coderacademy.edu.au>

--- src/components/meals-components/Meals.js
+++ src/components/meals-components/Meals.js
@@ -1,51 +1,55 @@
-import Table from 'react-bootstrap/Table';
-import React from 'react';
-import ViewMeal from './ViewMeal';
-import { useGlobalState } from '../../config/store';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import DatePicker from 'react-datepicker';
+
+import moment from 'moment';
+import React from 'react';
+// import ViewMeal from './ViewMeal';
+import { useGlobalState } from '../../config/store';
 
 const Meals = () => {
 	const { store } = useGlobalState();
 	const { meals } = store;
-	// for table heading
-	const tableHeadings = [
-		'Title',
-		'Description',
-		'Meal Type',
-		'Cost',
-		'Delivers On',
-		'Order Starts',
-		'Order Ends',
-		'Max Orders'
-	];
-	// Array for meal details
-	const mealHeadings = [
-		'title',
-		'description',
-		'mealType',
-		'cost',
-		'deliversOn',
-		'orderStarts',
-		'orderEnds',
-		'maxOrders'
-	];
+
+	const headings = {
+		title: { heading: 'Title' },
+		description: { heading: 'Description' },
+		mealType: { heading: 'Meal Type' },
+		cost: { heading: 'Cost' },
+		deliversOn: { heading: 'Delivers On', type: 'date' },
+		orderStarts: { heading: 'Order Starts', type: 'date' },
+		orderEnds: { heading: 'Order Ends', type: 'date' },
+		maxOrders: { heading: 'Max Orders' }
+	};
 	return (
 		<Container>
 			<Row className="justify-content-center">
-				<h3>Welcome to the Homemade Meals</h3>
+				<h3>Meals History</h3>
 				<Table striped bordered hover>
 					<thead>
 						<tr>
 							<th>#</th>
-							{tableHeadings.map((heading) => <th key={heading}>{heading}</th>)}
+							{Object.values(headings).map(({ heading }) => (
+								<th className="text-capitalize" key={heading}>
+									{heading}
+								</th>
+							))}
 						</tr>
 					</thead>
 					<tbody>
 						{meals.sort((a, b) => new Date(b.deliversOn) - new Date(a.deliversOn)).map((meal, index) => (
 							<tr key={meal._id}>
 								<td>{index + 1}</td>
-								{mealHeadings.map((heading) => <td>{meal[heading]}</td>)}
+								{Object.keys(headings).map((heading) => (
+									<td>
+										{headings[heading].type === 'date' ? (
+											moment(meal[heading]).format('MMMM d, yyyy h:mm a')
+										) : (
+											meal[heading]
+										)}
+									</td>
+								))}
 							</tr>
 						))}
 					</tbody>

eb67e1c16c9623467591a8e0538a149f60833f6d
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-07-26 00:56:31 +1000

    role based authorization

--- src/components/auth-components/AuthenticatedRoute.js
+++ src/components/auth-components/AuthenticatedRoute.js
@@ -1,22 +1,33 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 import { useGlobalState } from '../../config/store'
-// import {getLoggedInUser} from '../../services/authServices'
 
-export default ({ component: Component, location, redirectMsg, ...rest }) => {
-  const { loggedInUser } = useGlobalState()
+export default ({ component: Component, location, redirectMsg, role, ...rest }) => {
+  const { loggedInUser, dispatch } = useGlobalState()
+
+  if (!loggedInUser) {
+    return <Redirect to={
+      {
+        pathname: '/login',
+        state: {
+          referrer: location.pathname,
+          msg: redirectMsg
+        }
+      }
+    } />
+  }
+
+  if (typeof (role) !== "undefined" && loggedInUser.role !== role) {
+    dispatch({
+      type: "setError",
+      data: {
+        title: "Sorry that action is not allowed.",
+        msg: `As a ${loggedInUser.role }, you can not access that path.`
+      }
+    })
+  }
 
   return (
-    loggedInUser ?
-      <Route {...rest} location render={(props) => <Component {...props} />} /> :
-      <Redirect to={
-        {
-          pathname: '/login',
-          state: {
-            referrer: location.pathname,
-            msg: redirectMsg
-          }
-        }
-      } />
+      <Route {...rest} location={location} render={(props) => <Component {...props} />} />      
   )
 }

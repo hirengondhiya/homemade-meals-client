2595347a67ad3d6136bfbc62f1e1fb95639be9ee
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-07-27 22:47:11 +1000

    home page component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,8 @@
 import Spinner from 'react-bootstrap/Spinner';
 
 import React from 'react'
-import ViewMeal from './meals-components/ViewMeal'
-import Meal from './meals-components/Meal'
+import MealCustomerView from './meals-components/HomeCustomer'
+import MealSellerView from './meals-components/HomeSeller'
 
 import { useGlobalState } from '../config/store'
 
@@ -18,22 +18,30 @@ export default function Home() {
       <Spinner animation="border" role="status">
         <span className="sr-only">Loading...</span>
       </Spinner>
-    )    
+    )
   }
 
   let MealComponent;
+  let msg = ""
   // when logged in user is seller
   // home page shows meals due to be delivered in ascending order
   if (loggedInUser && loggedInUser.role === "seller") {
-    MealComponent = ViewMeal
+    MealComponent = MealSellerView
     mealsData = meals.filter((meal) => meal.dueSoon).sort((a, b) => new Date(a.deliversOn) - new Date(b.deliversOn))
+    if (mealsData.length === 0) {
+      msg = "You don't have any meals to deliver"
+    }
   } else {
-    MealComponent = Meal
+    MealComponent = MealCustomerView
     // otherwise show meals in based on orders closing
     mealsData = meals.sort((a, b) => new Date(a.orderEnds) - new Date(b.orderEnds))
+    if (mealsData.length === 0) {
+      msg = "We do not have any meals that can be ordered now. Please visit later."
+    }
   }
 
   return (
-    mealsData.map((mealData) => <MealComponent key={mealData._id} mealData={mealData} />)
+    msg ? <h2>{msg}</h2> :
+      mealsData.map((mealData) => <MealComponent key={mealData._id} mealData={mealData} />)
   )
 }

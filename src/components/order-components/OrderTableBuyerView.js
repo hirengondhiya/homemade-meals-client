04a0d6e278c55a9952afde0731452fbe2a7b1fcb
Niraj <amatyaraj@gmail.com>
2020-07-29 09:14:35 +1000

    global state

--- src/components/order-components/OrderTableBuyerView.js
+++ src/components/order-components/OrderTableBuyerView.js
@@ -6,3 +6,4 @@ import Spinner from 'react-bootstrap/Spinner';
 import moment from 'moment';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useGlobalState } from '../../config/store';

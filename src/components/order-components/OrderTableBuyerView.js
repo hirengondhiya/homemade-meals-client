6e2a37f4ea2e9fc7f6d848fa9a88f7337fa50ba2
Niraj <amatyaraj@gmail.com>
2020-07-29 09:13:56 +1000

    imported react and react bootstrap

---
+++ src/components/order-components/OrderTableBuyerView.js
@@ -0,0 +1,8 @@
+import Row from 'react-bootstrap/Row';
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import Spinner from 'react-bootstrap/Spinner';
+
+import moment from 'moment';
+import React from 'react';
+import { Link } from 'react-router-dom';

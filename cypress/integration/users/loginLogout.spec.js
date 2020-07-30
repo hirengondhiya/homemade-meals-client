e60bfb72c59e6e42cc5cdf3debbc9f97d44ccc7a
Niraj <amatyaraj@gmail.com>
2020-07-30 16:20:11 +1000

    test for viewport size

--- cypress/integration/users/loginLogout.spec.js
+++ cypress/integration/users/loginLogout.spec.js
@@ -1,5 +1,6 @@
 describe('Test login', () => {
 	it('should go to /login', () => {
+		cy.viewport(1024, 768);
 		cy.visit('/');
 		cy.get('[data-cy=login]').click();
 		cy.url().should('contains', '/login');

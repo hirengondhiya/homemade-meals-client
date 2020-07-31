be87c550a898f25db11781339d2c07f83aaae442
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-07-31 20:37:02 +1000

    recordings for integration testing

--- cypress/integration/users/loginLogout.spec.js
+++ cypress/integration/users/loginLogout.spec.js
@@ -1,41 +1,49 @@
 let fixtures = {};
 
-before(() => {
-  cy.fixture("registeredUser.json").then((user) => {
-    fixtures.registeredUser = user;
+describe("Test Login/Logout users path", () => {
+  before(() => {
+    cy.fixture("registeredUser.json").then((user) => {
+      fixtures.registeredUser = user;
+    });
   });
-});
 
-beforeEach(() => {
-  cy.viewport(1024, 768);
-  cy.visit("/");
-});
+  beforeEach(() => {
+    cy.viewport(1024, 768);
+    cy.visit("/");
+  });
 
-describe("Test login", () => {
-  it("should go to /login", () => {
-    cy.get("[data-cy=login]").click();
-    cy.url().should("contains", "/login");
+  describe("Test login", () => {
+    beforeEach(() => {
+      cy.get('[data-cy="login"]').click();
+    });
+    it("should go to /login", () => {
+      cy.url().should("contains", "/login");
+    });
+    it("should render the signIn component", () => {
+      cy.get("[data-cy=login-form]").should("be.visible");
+    });
+    describe("Submit login form", () => {
+      afterEach(() => {
+        cy.get("[data-cy=logout]").click();
+      });
+      it("should successfully login", () => {
+        const { username, password } = fixtures.registeredUser;
+        cy.get("[data-cy=username]").type(username);
+        cy.get("[data-cy=password]").type(password);
+        cy.get("[data-cy=login-button]").click();
+        cy.get("[data-cy=logout]").should("be.visible");
+      });
+    });
   });
-  it("should render the signIn component", () => {
-    cy.get("[data-cy=login]").click();
-    cy.get("[data-cy=login-form]").should("be.visible");
-  });
-  it("should successfully login", () => {
-    const { username, password } = fixtures.registeredUser;
-    cy.get("[data-cy=login]").click();
-    cy.get("[data-cy=username]").type(username);
-    cy.get("[data-cy=password]").type(password);
-    cy.get("[data-cy=login-button]").click();
-    cy.get("[data-cy=logout]").should("be.visible");
-  });
-});
-describe("Test logout", () => {
-  it("should successfully logout", () => {
-    const { username, password } = fixtures.registeredUser;
-    cy.get("[data-cy=login]").click();
-    cy.get("[data-cy=username]").type(username);
-    cy.get("[data-cy=password]").type(password);
-    cy.get("[data-cy=login-button]").click();
-    cy.get("[data-cy=logout]").click();
+  describe("Test logout", () => {
+    beforeEach(() => {
+      // const { username, password } = fixtures.registeredUser;
+      // cy.get("[data-cy=username]").type(username);
+      // cy.get("[data-cy=password]").type(password);
+      // cy.get("[data-cy=login-button]").click();
+    });
+    it("should successfully logout", () => {
+      cy.get('[data-cy="logout"]').click();
+    });
   });
 });
--- cypress/integration/users/register.spec.js
+++ cypress/integration/users/register.spec.js
@@ -1,7 +1,7 @@
 import { v4 as uuid } from "uuid";
 let fixtures = {};
 
-describe("Test Register", () => {
+describe("Test Register path", () => {
   beforeEach(() => {
     cy.viewport(1024, 768);
     cy.visit("/");
@@ -13,51 +13,46 @@ describe("Test Register", () => {
   it("should render register component", () => {
     cy.get('[data-cy="register-form"]').should("be.visible");
   });
-  it("should give error on not matching password", () => {
-    const username = uuid();
-    cy.get('[data-cy="register-form"]').within(() => {
-      cy.get('[data-cy="username"]').type(username);
-      cy.get('[data-cy="email"]').type(`${username}@email.com`);
-      cy.get('[data-cy="radio-seller"]').click();
-      cy.get('[data-cy="password"]').type(username);
-      cy.get('[data-cy="confirm-password"]').type(username + "1");
+
+  describe("on submit", () => {
+    beforeEach(() => {
+      const username = uuid();
+      cy.get('[data-cy="register-form"]').within(() => {
+        cy.get('[data-cy="username"]').type(username);
+        cy.get('[data-cy="email"]').type(`${username}@email.com`);
+        cy.get('[data-cy="radio-seller"]').click();
+        cy.get('[data-cy="password"]').type(username);
+        cy.get('[data-cy="confirm-password"]').type(username);
+      });
+    });
+    it("should give error on not matching password", () => {
+      cy.get('[data-cy="confirm-password"]').type("abcd");
       cy.get('[data-cy="submit-button"]').click();
       cy.get('[data-cy="validatio-error"]').should(
         "have.text",
         "Password does not match. Please try again."
       );
     });
-  });
-  it("should register seller", () => {
-    const username = uuid();
-    cy.get('[data-cy="register-form"]').within(() => {
-      cy.get('[data-cy="username"]').type(username);
-      cy.get('[data-cy="email"]').type(`${username}@email.com`);
+    it("should register seller", () => {
+      const username = uuid();
       cy.get('[data-cy="radio-seller"]').click();
-      cy.get('[data-cy="password"]').type(username);
-      cy.get('[data-cy="confirm-password"]').type(username);
       cy.get('[data-cy="submit-button"]').click();
+      cy.get('[data-cy="alert-success"] [data-cy="alert-msg"]').should(
+        "contain",
+        "Welcome to Homemade meals,"
+      );
+      cy.get('[data-cy="meals"]').should("have.text", "Meals history");
+      cy.get('[data-cy="logout"]').click();
     });
-    cy.get('[data-cy="alert-success"] [data-cy="alert-msg"]').should(
-      "contain",
-      username
-    );
-    cy.get('[data-cy="meals"]').should("have.text", "Meals history");
-  });
-  it("should register buyer", () => {
-    const username = uuid();
-    cy.get('[data-cy="register-form"]').within(() => {
-      cy.get('[data-cy="username"]').type(username);
-      cy.get('[data-cy="email"]').type(`${username}@email.com`);
+    it("should register buyer", () => {
       cy.get('[data-cy="radio-buyer"]').click();
-      cy.get('[data-cy="password"]').type(username);
-      cy.get('[data-cy="confirm-password"]').type(username);
       cy.get('[data-cy="submit-button"]').click();
+      cy.get('[data-cy="alert-success"] [data-cy="alert-msg"]').should(
+        "contain",
+        "Welcome to Homemade meals,"
+      );
+      cy.get('[data-cy="orders"]').should("have.text", "My Orders");
+      cy.get('[data-cy="logout"]').click();
     });
-    cy.get('[data-cy="alert-success"] [data-cy="alert-msg"]').should(
-      "contain",
-      username
-    );
-    cy.get('[data-cy="orders"]').should("have.text", "My Orders");
   });
 });
